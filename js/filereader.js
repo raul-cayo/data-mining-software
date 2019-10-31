3bfd148db89234ddd5ea6f5c0447ecdb96729415
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2019-10-31 06:48:03 -0600

    Improve filereader

--- js/filereader.js
+++ js/filereader.js
@@ -1,11 +1,38 @@
-function loadFileAsText(){
-  var fileToLoad = document.getElementById("fileToLoad").files[0];
+function bs_input_file() {
+  $(".input-file").before(
+    function () {
+      if (!$(this).prev().hasClass('input-ghost')) {
+        var element = $("<input id='fileToLoad' type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+        element.attr("name", $(this).attr("name"));
+        element.change(function () {
+          element.next(element).find('input').val((element.val()).split('\\').pop());
+        });
 
-  var fileReader = new FileReader();
-  fileReader.onload = function(fileLoadedEvent){
-      var textFromFileLoaded = fileLoadedEvent.target.result;
-      document.getElementById("filetext").value = textFromFileLoaded;
-  };
+        $(this).find("button.btn-choose").click(function () {
+          element.click();
+        });
 
-  fileReader.readAsText(fileToLoad, "UTF-8");
+        $(this).find("button.btn-submit").click(function () {
+          var fileToLoad = document.getElementById("fileToLoad").files[0];
+          var fileReader = new FileReader();
+          fileReader.onload = function (fileLoadedEvent) {
+            var textFromFileLoaded = fileLoadedEvent.target.result;
+            document.getElementById("filetext").value = textFromFileLoaded;
+          };
+          fileReader.readAsText(fileToLoad, "UTF-8");
+        });
+
+        $(this).find('input').css("cursor", "pointer");
+        $(this).find('input').mousedown(function () {
+          $(this).parents('.input-file').prev().click();
+          return false;
+        });
+        return element;
+      }
+    }
+  );
 }
+
+$(function () {
+  bs_input_file();
+});
